6581b069b726999feaf71b12bc3b6a4fbbccc3e5
SHUVO HALDER <42088593+cybsam@users.noreply.github.com>
2024-11-21 23:17:31 +0600

    adding sets

---
+++ Python/Sets/setUnion.py
@@ -0,0 +1,5 @@
+number_of_english_subscribers = input()
+english_subscribers = set(map(int, input().split()))
+number_of_french_subscribers = input()
+french_subscribers = set(map(int, input().split()))
+print(len(english_subscribers.union(french_subscribers)))
