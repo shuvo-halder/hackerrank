89c72797b6fe82385d724af6104ca19ca182202b
SHUVO HALDER <samshuvo00@gmail.com>
2024-12-18 03:11:46 +0600

    tupple and list

--- python-basic/list-tupple.py
+++ python-basic/list-tupple.py
@@ -9,3 +9,19 @@ health_after_attack = player_health - sword_damage
 print(f" sams health is: {player_health}")
 print(f"sword damage {sword_damage}")
 print(f"player health after sword damage : {health_after_attack}")
+
+
+
+# list
+listName = ["apple", "banana", "Number"]
+print(type(listName))
+print(listName[2])
+
+secondList = ['name','email','number','sam']
+print(type(secondList))
+print(secondList[3])
+
+# tupple
+tuppleNameList = ('tupple1','tupple2','tupple3')
+print(type(tuppleNameList)) 
+print(tuppleNameList[1])
