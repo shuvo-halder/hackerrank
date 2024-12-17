968fce2c1d85af7bb8e96b72f716100b60785cdd
SHUVO HALDER <samshuvo00@gmail.com>
2024-12-18 03:19:11 +0600

    same #variable but #defferent value

---
+++ python-basic/samevariable.py
@@ -0,0 +1,24 @@
+import os, sys
+
+# we are showing beauty of variable defining python
+# suppose player health is default 1000
+
+player_default_health = 1000
+player_health = player_default_health - 100
+
+# printing heating after health
+print(player_health)
+
+# again define player halth
+player_health = 800
+print(player_health)
+
+# again define player health is 700
+player_health = 700
+print(player_health)
+
+# again define player health is 600
+player_health = 600
+print(player_health)
+
+
