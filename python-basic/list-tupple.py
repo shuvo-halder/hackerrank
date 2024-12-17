9ef8fa3c7077963e8dda1e10929102809590e937
SHUVO HALDER <samshuvo00@gmail.com>
2024-12-18 02:58:26 +0600

    boot.dev first program

---
+++ python-basic/list-tupple.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+
+sword_damage = 10
+player_health = 100
+health_after_attack = player_health - sword_damage
+
+print(f" sams health is: {player_health}")
+print(f"sword damage {sword_damage}")
+print(f"player health after sword damage : {health_after_attack}")
