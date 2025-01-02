cb85f7fa68840e9b1230d26ec6ab045c2387c1a1
SHUVO HALDER <samshuvo00@gmail.com>
2025-01-02 23:43:17 +0600

    this should be correct code

--- Python/list/list.py
+++ Python/list/list.py
@@ -1,26 +1,30 @@
 if __name__ == '__main__':
     N = int(input())
 
-    ar = []
-    for _ in range(N):
-        command_arg = input().strip().split(" ")
-        cmd = command_arg[0]
-
-        if cmd == "print":
-            print(ar)
-        elif cmd == "short":
-            ar.sort()
-        elif cmd == "reverse":
-            ar.reverse()
-        elif cmd == "pop":
-            ar.pop()
-        elif cmd == "remove":
-            val = int(command_arg[1])
-            ar.remove(val)
-        elif cmd == "append":
-            val = int(command_arg[1])
-            ar.append(val)
-        elif cmd == "insert":
-            val = int(command_arg[2])
-            pos = int(command_arg[1])
-            ar.insert(pos, val)
+    l = []
+    for i in range(N):
+        name = input()
+        spl = str.split(name)
+        if len(spl) == 3:
+            com = str(spl[0])
+            ind = int(spl[1])
+            obj = int(spl[2])
+        elif len(spl) == 2:
+            com = str(spl[0])
+            obj = int(spl[1])
+        elif len(spl) == 1:
+            com = str(spl[0])
+        if com == "insert":
+            l.insert(ind,obj)
+        elif com == "print":
+            print (l)
+        elif com == "remove":
+            l.remove(obj)
+        elif com == "append":
+            l.append(obj)
+        elif com == "sort":
+            l.sort()
+        elif com == "pop":
+            l.pop()
+        else:
+            l.reverse()
